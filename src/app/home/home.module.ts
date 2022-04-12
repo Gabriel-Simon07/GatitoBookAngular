acc99ce9b3d236e9361c7c3b24516264200d31ac
Gabriel Simon <gabrielsimon775@gmail.com>
2022-04-11 21:30:41 -0300

    User registration screen is being implemeting

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,22 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { AppRoutingModule } from '../app-routing.module';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
-import { MensagemComponent } from '../componentes/mensagem/mensagem.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MensagemModule } from '../componentes/mensagem/mensagem.module';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent, LoginComponent
+    HomeComponent, LoginComponent, NovoUsuarioComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     FormsModule,
-    MensagemModule
+    MensagemModule,
+    ReactiveFormsModule
   ],
   exports: [HomeComponent]
 })
---
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+
+@Component({
+  selector: 'app-novo-usuario',
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.css']
+})
+export class NovoUsuarioComponent implements OnInit {
+
+  novoUsuarioForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    // private novoUsuarioService: NovoUsuario
+    ) { }
+
+  ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      userName: [''],
+      fullName: [''],
+      password: ['']
+    });
+  }
+
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
+  }
+}
