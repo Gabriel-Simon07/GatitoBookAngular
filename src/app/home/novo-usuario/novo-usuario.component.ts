30c52d3aa2e4cf373b1634841247a79ef3e4b1ba
Gabriel Simon <gabrielsimon775@gmail.com>
2022-04-22 21:53:40 -0300

    Validator of password and username cant equals

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { minusculoValidator } from './minusculo.validatitor';
 import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 import { UsuarioExiste } from './usuario-existe.service';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -15,8 +18,9 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private usuarioJaExistenteService: UsuarioExiste
-    // private novoUsuarioService: NovoUsuario
+    private usuarioJaExistenteService: UsuarioExiste,
+    private novoUsuarioService: NovoUsuarioService,
+    private router: Router
     ) { }
 
   ngOnInit(): void {
@@ -25,11 +29,23 @@ export class NovoUsuarioComponent implements OnInit {
       userName: ['', [minusculoValidator], [this.usuarioJaExistenteService.usuarioJaExiste()]],
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       password: ['']
-    });
+    }, {
+      validador: ['', [usuarioSenhaIguaisValidator]],
+    }
+    );
   }
 
   cadastrar() {
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario);
+    if(this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(() => {
+        this.router.navigate(['']);
+      },
+      (error) => {
+        console.log(error);
+      });
+    }
+
+
   }
 }
