32b072b4b4d4fd0e574c75e9ebddd7cbc473d5f6
Gabriel Simon <gabrielsimon775@gmail.com>
2022-04-25 21:21:34 -0300

    Rollback commit 12

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { minusculoValidator } from './minusculo.validatitor';
-import { NovoUsuario } from './novo-usuario';
-import { NovoUsuarioService } from './novo-usuario.service';
 import { UsuarioExiste } from './usuario-existe.service';
-import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -18,9 +14,7 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private usuarioJaExistenteService: UsuarioExiste,
-    private novoUsuarioService: NovoUsuarioService,
-    private router: Router
+    private usuarioJaExistenteService: UsuarioExiste
     ) { }
 
   ngOnInit(): void {
@@ -29,23 +23,7 @@ export class NovoUsuarioComponent implements OnInit {
       userName: ['', [minusculoValidator], [this.usuarioJaExistenteService.usuarioJaExiste()]],
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       password: ['']
-    }, {
-      validador: ['', [usuarioSenhaIguaisValidator]],
     }
     );
-  }
-
-  cadastrar() {
-    if(this.novoUsuarioForm.valid) {
-      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(() => {
-        this.router.navigate(['']);
-      },
-      (error) => {
-        console.log(error);
-      });
-    }
-
-
   }
 }
