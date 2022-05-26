3a76dace2d1da5f13f6bcaf844abf63eff779bcb
Gabriel Simon <gabrielsimon775@gmail.com>
2022-05-25 21:51:58 -0300

    UserService has been implemented

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/autenticacao/usuario/usuario';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { AnimaisService } from '../animais.service';
+import { Animais } from '../animal';
 
 @Component({
   selector: 'app-lista-animais',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  constructor() { }
+  animais!:Animais;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+    ) { }
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name?? '';
+      this.animaisService.listaUsuario(userName).subscribe((animais) => {
+        this.animais = animais;
+      })
+    })
   }
 
 }
