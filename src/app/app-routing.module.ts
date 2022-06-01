f455f3fa12d85c6e736af5496ce795cab6368cb8
Gabriel Simon <gabrielsimon775@gmail.com>
2022-06-01 20:33:32 -0300

    Details of animal component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AutenticacaoGuard } from "./autenticacao/autenticacao.guard";
+import { LoginGuard } from "./autenticacao/login.guard";
 
 const routes: Routes = [
   {
@@ -8,13 +10,15 @@ const routes: Routes = [
     redirectTo: 'home',
   },
   {
-    path:'home',
-    loadChildren:() => import('./home/home.module').then((module) => module.HomeModule),
-  },
-  {
     path:'animais',
     loadChildren:() => import('./animais/animais.module').then((module) => module.AnimaisModule),
-  }
+    canLoad: [AutenticacaoGuard]
+  },
+  {
+    path:'home',
+    loadChildren:() => import('./home/home.module').then((module) => module.HomeModule),
+    canLoad: [LoginGuard]
+  },
 ];
 
 @NgModule({
